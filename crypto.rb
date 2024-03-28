ec522831cbb856477466c07f80e8c8d805527cc4
John Scott <jfscott@memphis.edu>
2024-03-27 23:24:59 -0500

    not complete

--- crypto.rb
+++ crypto.rb
@@ -2,7 +2,15 @@ require "httparty"
 
 response = HTTParty.get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd")
 
-pp response.headers
+response.each do | line |
+    # line.each do | name, current_price, market_cap |
+    #     puts "Name: %s Current Price: %f Market Cap: $d" [name, current_price, market_cap]
+    # end
+    line.each do | name |
+        pp line[name]
+    end
+end
+# pp response.headers
 
 # ▪ Parse the JSON response to extract data for each cryptocurrency,
 # including its name, current price, and market capitalization.
@@ -10,3 +18,4 @@ pp response.headers
 # ▪ Output: List the names and current prices of the top 5
 # cryptocurrencies by market capitalization, along with their
 # respective market cap values.
+# pp response.body
--- repo_analyzer.rb
+++ repo_analyzer.rb
@@ -1,11 +1,36 @@
 require "httparty"
 
-response = HTTParty.get("https://api.github.com/users/{/kamranahmedse/}/repos")
+response = HTTParty.get("https://api.github.com/users/kbridson/repos")
 
+# date = response['date']
+# pp response["server"]
 # pp response.headers
+# puts response.match(/<title>(.*)<\/title>/)
+
+# puts response.code
+# pp response[0]["id"]
 
 # ▪ Parse the JSON response to extract information about each
 # repository, such as its name, star count, and fork count.
 # ▪ Find the repository with the highest star count.
+# pp response[0]
+
+
+
+
 # ▪ Output: Display the name, description, number of stars, and URL
 # of the most starred repository.
+# pattern = /"([^"]+)"\s*:\s*("[^"]*"|\d+|true|false|null)/
+# puts response.match(pattern)
+name = {}
+description = {}
+number_stars = {}
+url = {}
+response.map do | line |
+    name.append(response[line]["name"])
+    description.append(response[line]["description"])
+    number_stars.append(response[line]["number_stars"])
+    url.append(response[line]["url"])
+
+end
+
